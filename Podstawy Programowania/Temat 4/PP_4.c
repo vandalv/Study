45e70d8c32b4f1a8d839d8bb208eade7d33d3a31
vandalv <vandalv@tut.by>
2020-05-08 12:05:42 +0200

    4_4 - partly

--- Podstawy Programowania/Temat 4/PP_4.c
+++ Podstawy Programowania/Temat 4/PP_4.c
@@ -6,13 +6,19 @@ int main()
 {
    double a,b, summ,subtract,multiply,divide,residue;
      char c;
-     int k = 1;
+     int k = 0;
      do{
        printf("Please, Enter Operation You Want To Perform \"+\", \"-\", \"*\", \"/\", \"%%\":\n");
        scanf("%c", &c);
        getchar();
        system("CLS");
      } while((c != '+') && (c != '-') && (c != '*') && (c != '/') && (c != '%'));
+     do{
+       printf("The Operator You Choose Is - \"%c\". Please, Enter Two Operands To Continue\n", c);
+       k = scanf("%lf %lf", &a, &b);
+       fflush(stdin);
+       system("CLS");
+     } while(k == 0);
 
    return 0;
 }
