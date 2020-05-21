242ff49132ff01c8860df7fa49badbf89edaf317
vandalv <vandalv@tut.by>
2020-05-21 09:50:38 +0200

    PP_5a_1.c - complete!

--- Podstawy Programowania/Temat 5/PP_5a_1.c
+++ Podstawy Programowania/Temat 5/PP_5a_1.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
-int main(void){
-  int i = 10;
-  int *p, *q;
-  *p = &i;
-  printf("%d", *p);
-  return 0;
+#define MAX_SIZE 100
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int N, i;
+    int *ptr = arr;
+    printf("Enter Size Of Array: ");
+    scanf("%d", &N);
+    printf("Enter Elements In Array:\n");
+    for (i = 0; i < N; i++)
+    {
+        scanf("%d", ptr);
+        ptr++;
+    }
+    ptr = arr;
+    printf("Array Elements Are:\n");
+    for (i = 0; i < N; i++)
+    {
+        printf("%d\n", *ptr);
+
+        ptr++;
+    }
+    return 0;
 }
