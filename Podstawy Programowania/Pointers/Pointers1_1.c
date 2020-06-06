9e779f1c10b1b63eb303295ae5233bd253fd478a
vandalv <vandalv@tut.by>
2020-06-06 16:48:46 +0200

    pointers beggining

---
+++ Podstawy Programowania/Pointers/Pointers1_1.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+int num;
+int *numPtr;
+int num2;
+num = 13;
+numPtr = &num;
+num2 = *numPtr;
+printf("%d - num, %d - numPtr, %d - adress of num, %d - num2", num, numPtr, &num, &num2);
+
+  return 0;
+}
