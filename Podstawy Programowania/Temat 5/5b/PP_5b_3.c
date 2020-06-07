842fe6a3bc1d40fd0d2f0ec837f26c52415af155
vandalv <vandalv@tut.by>
2020-06-07 15:41:42 +0200

    5b_3

--- Podstawy Programowania/Temat 5/5b/PP_5b_3.c
+++ Podstawy Programowania/Temat 5/5b/PP_5b_3.c
@@ -1,23 +1,20 @@
 # include <stdio.h>
 
-int argCalc(int *a, int *b, int *summ, int *rozn);
+int argCalc(int *a, int *b, int *summ, int *rozn, int *mult);
 
 int main(){
-  int a,b,summ,rozn,mult;
+  int a,b,summ,rozn, mult;
   printf("Input argument a\n");
   scanf("%d", &a);
   printf("Input argument b\n");
   scanf("%d", &b);
-  mult = argCalc(&a, &b, &summ, &rozn);
-  printf("%d %d", summ,rozn);
-  printf("%d", mult);
+  argCalc(&a, &b, &summ, &rozn, &mult);
+  printf("%d %d %d", summ,rozn,mult);
   return 0;
 }
 
-int argCalc(int *a, int *b, int *summ, int *rozn){
-  int mult;
+int argCalc(int *a, int *b, int *summ, int *rozn, int *mult){
   *summ = *a + *b;
   *rozn = *a - *b;
-  mult = *a * *b;
-  return(mult);
+  *mult = *a * *b;
 }
