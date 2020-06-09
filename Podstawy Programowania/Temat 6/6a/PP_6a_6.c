38a50aec1ca5059a13edee3dfe09853e20d2b27d
vandalv <vandalv@tut.by>
2020-06-09 16:18:58 +0200

    allst

--- Podstawy Programowania/Temat 6/6a/PP_6a_6.c
+++ Podstawy Programowania/Temat 6/6a/PP_6a_6.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(){
-  int ch;
-  while((ch=getchar()) != EOF);
-  system("PAUSE");
-  return 0;
+int main()
+{
+    char pass[8];
+    char correct[4] = "sky";
+    int i = 0;
+    int flag = 0;
+    int flag2 = 3;
+    char* pass_ptr = NULL;
+    pass_ptr = pass;
+    while(flag2 > 0){
+    fgets(pass, 6, stdin);
+    for(i; i<3;i++){
+        if(*pass_ptr == correct[i]){
+            pass_ptr++;
+            flag += 1;
+        }
+        else{
+            break;
+        }
+    }
+    if(flag == 3){
+        printf("Password Is Correct");
+        flag2 = 0;
+    }
+    else{
+        printf("Password Is Wrong\n");
+        flag2 -= 1;
+    }
+    }
+    return  0;
 }
---
+++ Podstawy Programowania/Temat 6/6b/PP_6b_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char surname[10];
+  char name[10];
+  fget(surname, 10, stdin);
+  printf(surname);
+}
