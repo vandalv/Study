46dcbc2184c74ef15b3bc8f58ba407ceec4250f5
vandalv <vandalv@tut.by>
2020-06-10 13:55:59 +0200

    PP_6b_3a

--- Podstawy Programowania/Temat 6/6b/PP_6b_3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-  char surname[10];
-  char name[10];
-  fget(surname, 10, stdin);
-  printf(surname);
-}
---
+++ Podstawy Programowania/Temat 6/6b/PP_6b_3a.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define SIZE 10
+
+void lower(char* p_surname);
+
+int main(){
+  char surname[SIZE-1];
+  char name[SIZE-1];
+  char* p_surname = NULL;
+  char* p_name = NULL;
+  p_surname = surname;
+  p_name = name;
+  fgets(surname, SIZE-1, stdin);
+  lower(p_surname);
+  fputs(surname,stdout);
+  fgets(name, SIZE-1, stdin);
+  lower(p_name);
+  fputs(name,stdout);
+}
+
+void lower(char* p_surname){
+  while(*p_surname){
+    *p_surname = tolower(*p_surname);
+    *p_surname++;
+  }
+}
