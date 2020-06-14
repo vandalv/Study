d55e44f2bb5e44195329b7ee160064d2f46ea34f
vandalv <vandalv@tut.by>
2020-06-14 12:55:02 +0200

    8_!

--- Podstawy Programowania/Temat 8/8a/PP_8a_1.c
+++ Podstawy Programowania/Temat 8/8a/PP_8a_1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-
-int main(){
-  FILE *file_ptr;
-  file_ptr = fopen("new_file.txt","w+");
-  if(file_ptr!=0){
-    printf("File has been created");
-  }
-  else{
-    printf("File has not been created");
-  }
-  fwrite(arr, sizeof(arr), 1, file_ptr);
-}
+#include <conio.h>
+int main()
+ { int i1=5,i2=10,i3=15,buf;
+ FILE *fp;
+ fp=fopen("Plik1.dat","w+b");
+ if (fp==NULL)
+ {
+ printf("\n Plik nie daje sie utworzyc");
+ return 1;
+ }
+ fwrite(&i1,sizeof(int),1,fp);
+ fwrite(&i2,sizeof(int),1,fp);
+ fwrite(&i3,sizeof(int),1,fp);
+ fseek(fp,0,SEEK_SET);
+ fread(&buf,sizeof(int),1,fp);
+ printf("\n Element nr 1 =%d",buf);
+ fread(&buf,sizeof(int),1,fp);
+ printf("\n Element nr 2 =%d",buf);
+ fread(&buf,sizeof(int),1,fp);
+ printf("\n Element nr 3 =%d\n",buf);
+ fclose(fp);
+ return 1;} 
