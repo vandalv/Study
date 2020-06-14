825d6139104f250decbd893e9da8f75b37215108
vandalv <vandalv@tut.by>
2020-06-14 11:53:45 +0200

    1

--- Podstawy Programowania/Temat 8/8a/PP_8a_1.c
+++ Podstawy Programowania/Temat 8/8a/PP_8a_1.c
@@ -2,5 +2,12 @@
 
 int main(){
   FILE *file_ptr;
-  file_ptr = fopen("new_file.txt","w");
+  file_ptr = fopen("new_file.txt","w+");
+  if(file_ptr!=0){
+    printf("File has been created");
+  }
+  else{
+    printf("File has not been created");
+  }
+  fwrite(arr, sizeof(arr), 1, file_ptr);
 }
