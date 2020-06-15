35fc9cb006af5833e1ad1e18cbf4d5ed2e9a703a
vandalv <vandalv@tut.by>
2020-06-15 11:38:49 +0200

    1

--- Podstawy Programowania/Temat 8/8a/PP_8a_6.c
+++ Podstawy Programowania/Temat 8/8a/PP_8a_6.c
@@ -4,8 +4,8 @@
 
 int main()
  {
- int i,j;
- double tabBuf[5], tab[5]={1,7,3,4,5}, num, finarr[5];
+ int i,j,zam;
+ double tabBuf[5], tab[5]={1,7,3,4,5}, num, finarr[5], p1,p2, buf;
  double* arr;
  arr = (double*) malloc(5 * sizeof(double));
  FILE *fp;
@@ -40,6 +40,29 @@ int main()
  printf("\n Element file5.dat nr %d =%5.2f",i+1,tabBuf[i]);
  }
  fclose(fp);
+ fp=fopen("file5.dat","w+b");
+ for(i=0;i<=5;i++)
+ fwrite(&tab[i],sizeof(double),1,fp);
+ do { zam=0;
+ for( i=0;i<4;i++) {
+ fseek(fp,i*sizeof(double),SEEK_SET);
+ fread(&p1,sizeof(double),1,fp);
+ fread(&p2,sizeof(double),1,fp);
+ if (p1>p2)
+ {
+ fseek(fp,i*sizeof(double),SEEK_SET);
+ fwrite(&p2,sizeof(double),1,fp);
+ fwrite(&p1,sizeof(double),1,fp);
+ zam=1;
+ }
+ }
+} while(zam==1);
+fseek(fp,0,SEEK_SET);
+i=0;
+printf("\n Sorted File");
+while(fread(&buf,sizeof(double),1,fp))
+printf("\n Element nr %d=%.2lf",i++,buf);
+fclose(fp);
  getch();
  return 0;
 }
