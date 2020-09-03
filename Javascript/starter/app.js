72ac04a696f42eb624302dc482d974f24dc1d082
vandalv <vandalv@tut.by>
2020-09-04 01:23:17 +0200

    commit

--- Javascript/starter/app.js
+++ Javascript/starter/app.js
@@ -1,6 +1,20 @@
 let scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
+activePlayer = 1;
+dice = Math.floor(Math.random() * (6)) + 1;
 console.log(dice);
+
+// ! DOM text change from selecting element by ID
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = dice;
+
+// ! DOM get value
+
+let x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+// ! CSS change
+
+document.querySelector('.dice').style.display = 'none';
