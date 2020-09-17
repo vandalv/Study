52f6a3c8cea171b60ecd6cdbec05bbeeb8966b3a
vandalv <vandalv@tut.by>
2020-09-17 15:59:17 +0200

    init

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,7 +1,12 @@
 // Function Constructor
 
-let Person = function (name, yearOfBirth, job) {
+const Person = () => (name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
+  this.calculateAge = () => {
+    console.log(2020 - yearOfBirth);
+  };
 };
+
+let john = new Person('John', 1990, 'teacher');
