df303e02b55a019b39f1acaa73b4f2befee5792d
vandalv <vandalv@tut.by>
2020-09-17 16:44:02 +0200

    init

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,12 +1,23 @@
 // Function Constructor
 
-const Person = (name, yearOfBirth, job) => {
+function Person(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = () => {
-    console.log(2020 - yearOfBirth);
-  };
+}
+
+Person.prototype.calculateAge = function () {
+  console.log(2020 - this.yearOfBirth);
 };
 
+Person.prototype.lastname = 'Smith';
+
 const john = new Person('John', 1990, 'teacher');
+const mark = new Person('Mark', 1946, 'designer');
+const jane = new Person('Jane', 1910, 'retired');
+
+john.calculateAge();
+mark.calculateAge();
+jane.calculateAge();
+
+console.log(john);
